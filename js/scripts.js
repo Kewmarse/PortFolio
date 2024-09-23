6b55a642ea91c59c4fd98806371554558c306384
Jorissalmon <joris.salmon53290@gmail.com>
2024-09-23 12:00:08 +0200

    Netlify version 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -141,7 +141,7 @@ async function sendMessage(userMessage = null) {
 
      // Appel à l'API OpenAI pour obtenir une réponse
      try {
-        const response = await fetch('https://main--jorissalmon.netlify.app/functions/callopenai', {
+        const response = await fetch('https://main--jorissalmon.netlify.app/.netlify/functions/callopenai', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
