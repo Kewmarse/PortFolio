df2983e655840667150b9c8e0d087becad9e5db8
Jorissalmon <joris.salmon53290@gmail.com>
2024-09-23 16:27:46 +0200

    Essai Vercel

--- js/scripts.js
+++ js/scripts.js
@@ -154,7 +154,7 @@ async function sendMessage(userMessage = null) {
 
      // Appel à l'API OpenAI pour obtenir une réponse
      try {
-        const response = await fetch('https://porte-folio-eight.vercel.app/api/callopenai', {
+        const response = await fetch('https://porte-folio-kappa.vercel.app/api/callopenai', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
