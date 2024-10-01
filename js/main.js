467046a03334a1f40a5f44248022b0922d165109
Jorissalmon <joris.salmon53290@gmail.com>
2024-10-01 23:00:51 +0200

    modif index

--- js/main.js
+++ js/main.js
@@ -105,5 +105,19 @@
         portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
     
+    var blogIsotope = $('.blog .row').isotope({
+        itemSelector: '.blog-item',
+        layoutMode: 'fitRows'
+    });
+
+    // Gestion des clics sur les filtres du blog
+    $('#blog-filter li').on('click', function () {
+        $("#blog-filter li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+        
+        // Filtrer les éléments
+        blogIsotope.isotope({ filter: $(this).data('filter') });
+    });
+    
 })(jQuery);
 
