90d20b3316521fb3c8e471364e768b0b56c0aaf9
Joris Salmon <151244859+Jorissalmon@users.noreply.github.com>
2024-10-03 13:09:52 +0200

    Update callopenai.js

--- api/callopenai.js
+++ api/callopenai.js
@@ -2,12 +2,6 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-    // Autoriser les requêtes CORS
-
-
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Change '*' par ton domaine si tu veux restreindre
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === 'OPTIONS') {
         // Pour les requêtes préflight
